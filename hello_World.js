cb32b5a314d5571ebb8ac1b458bd467cffc73666
Adewale <a.adewalemuideen@gmail.com>
2026-07-16 14:10:33 +0100

    git ignore added

--- hello_World.js
+++ hello_World.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.json({ message: 'hello world' });
 });
 
 app.listen(3000, () => {
